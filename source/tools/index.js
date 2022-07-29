0f317599636c174021b25b7efcff4b9da31405b4
Said Ait Driss <saidaitdrissofficial@gmail.com>
2022-07-29 16:40:53 +0100

    Add files via upload

---
+++ source/tools/index.js
@@ -0,0 +1,37 @@
+export default {
+    requestOptions: {
+        //* set up your proxy here
+        // proxy: {
+        //     server: 'http://myproxy.com:3128',
+        //     username: 'usr',
+        //     password: 'pwd',
+        // },
+        headers: {
+            // 'Content-Type': 'application/json',
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            userAgent:
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+            accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        },
+    },
+    websites: [
+        {
+            name: 'playwright',
+            url: 'https://playwright.dev/docs/emulation',
+        },
+        {
+            name: 'proshop',
+            url:'https://www.proshop.se/'
+        }
+    ],
+    fileOptions: {
+        isNew: false, // true : create a new file and delete old one
+        htmlFile: true, // true : get html from targeted url
+        jsonFile: true, // true : store cookies in json file
+    },
+    browserOptions: {
+        name: 'chromium', // webkit, chromium, firefox
+        headless: false,
+        deviceName: 'Desktop Edge', // BlackBerry Z30, Desktop Edge, Galaxy Note 3 ,Nexus 4
+    },
+}
